3e1c0f52e10df92be56a73edc0f194cae5a980df
mikesplore <mikepremium8@gmail.com>
2024-09-18 15:31:56 +0300

    Initial Commit

---
+++ app/src/main/java/com/mike/hms/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.mike.hms.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFffffff)
+val Blue = Color (0XFF3FA2F6)
+val Red = Color(0XFFD20062)
+val WhiteSchemeTextColor = Color(0xff526D82)
+val BlackSchemeTextColor = Color(0xFF9DB2BF)
