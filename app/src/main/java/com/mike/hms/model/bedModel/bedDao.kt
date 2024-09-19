615e50fb9d2aa9f2428ca607fe8e59c948faba97
mikesplore <mikepremium8@gmail.com>
2024-09-19 13:50:00 +0300

    Backend Implementation

---
+++ app/src/main/java/com/mike/hms/model/bedModel/bedDao.kt
@@ -0,0 +1,27 @@
+package com.mike.hms.model.bedModel
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface BedDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBed(bed: BedEntity)
+
+    @Query("SELECT * FROM BedTable WHERE roomID = :roomID")
+    suspend fun getBedsByRoomID(roomID: String): List<BedEntity>
+
+    @Query("SELECT * FROM BedTable WHERE bedID = :bedID")
+    suspend fun getBedByID(bedID: String): BedEntity?
+
+    @Query("SELECT * FROM BedTable")
+    suspend fun getAllBeds(): List<BedEntity>
+
+    @Query("DELETE FROM BedTable WHERE bedID = :bedID")
+    suspend fun deleteBed(bedID: String)
+
+
+
+}
