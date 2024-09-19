93c825806ef3902fd15c37574d060e3cd2b9dd2d
mikesplore <mikepremium8@gmail.com>
2024-09-19 16:02:17 +0300

    Backend Implementation

--- app/src/main/java/com/mike/hms/model/roomModel/RoomViewModel.kt
+++ app/src/main/java/com/mike/hms/model/roomModel/RoomViewModel.kt
@@ -1,58 +1,102 @@
 package com.mike.hms.model.roomModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class RoomViewModel(private val roomRepository: RoomRepository) : ViewModel(){
+class RoomViewModel(private val roomRepository: RoomRepository) : ViewModel() {
 
-    fun insertRoom(room: RoomEntity) {
-        roomRepository.insertRoom(room)
+    private val _rooms = MutableLiveData <List<RoomEntity>>()
+    val rooms: LiveData<List<RoomEntity>> = _rooms
+
+    private val _room = MutableLiveData<RoomEntity>()
+    val room: LiveData<RoomEntity> = _room
+
+    private val _roomAllocations = MutableLiveData<List<RoomAllocationEntity>>()
+    val roomAllocations: LiveData<List<RoomAllocationEntity>> = _roomAllocations
+
+    private val _roomAllocation = MutableLiveData<RoomAllocationEntity>()
+    val roomAllocation: LiveData<RoomAllocationEntity> = _roomAllocation
+
+    private val _roomBookings = MutableLiveData<List<RoomBookingEntity>>()
+    val roomBookings: LiveData<List<RoomBookingEntity>> = _roomBookings
+
+    private val _roomBooking = MutableLiveData<RoomBookingEntity>()
+    val roomBooking: LiveData<RoomBookingEntity> = _roomBooking
+
+
+    fun insertRoom(room: RoomEntity, onSuccess: (Boolean) -> Unit) {
+        roomRepository.insertRoom(room) {
+            onSuccess(it)
+        }
     }
 
     fun getRoomByID(roomID: String) {
-        roomRepository.getRoomByID(roomID)
+        roomRepository.getRoomByID(roomID) {
+            _room.value = it
+        }
     }
 
     fun getAllRooms() {
-        roomRepository.getAllRooms()
-    }
-
-    fun deleteRoom(roomID: String) {
-        roomRepository.deleteRoom(roomID)
-    }
-
-    fun insertRoomAllocation(roomAllocation: RoomAllocationEntity) {
-        roomRepository.insertRoomAllocation(roomAllocation)
+        roomRepository.getAllRooms {
+            _rooms.value = it
         }
+    }
+
+    fun deleteRoom(roomID: String, onSuccess: (Boolean) -> Unit) {
+        roomRepository.deleteRoom(roomID) {
+            onSuccess(it)
+        }
+    }
+
+    fun insertRoomAllocation(roomAllocation: RoomAllocationEntity, onSuccess: (Boolean) -> Unit) {
+        roomRepository.insertRoomAllocation(roomAllocation) {
+            onSuccess(it)
+        }
+    }
 
     fun getRoomAllocationByID(roomAllocationID: String) {
-        roomRepository.getRoomAllocationByID(roomAllocationID)
+        roomRepository.getRoomAllocationByID(roomAllocationID) {
+            _roomAllocation.value = it
+        }
     }
 
     fun getAllRoomAllocations() {
-        roomRepository.getAllRoomAllocations()
+        roomRepository.getAllRoomAllocations {
+            _roomAllocations.value = it
+        }
 
     }
 
-    fun deleteRoomAllocation(roomAllocationID: String) {
-        roomRepository.deleteRoomAllocation(roomAllocationID)
+    fun deleteRoomAllocation(roomAllocationID: String, onSuccess: (Boolean) -> Unit) {
+        roomRepository.deleteRoomAllocation(roomAllocationID) {
+            onSuccess(it)
+        }
     }
 
-    fun insertRoomBooking(roomBooking: RoomBookingEntity) {
-        roomRepository.insertRoomBooking(roomBooking)
+    fun insertRoomBooking(roomBooking: RoomBookingEntity, onSuccess: (Boolean) -> Unit) {
+        roomRepository.insertRoomBooking(roomBooking) {
+            onSuccess(it)
+        }
     }
 
     fun getRoomBookingByID(roomBookingID: String) {
-        roomRepository.getRoomBookingByID(roomBookingID)
+        roomRepository.getRoomBookingByID(roomBookingID) {
+            _roomBooking.value = it
+        }
     }
 
     fun getAllRoomBookings() {
-        roomRepository.getAllRoomBookings()
+        roomRepository.getAllRoomBookings {
+            _roomBookings.value = it
+        }
     }
 
-    fun deleteRoomBooking(roomBookingID: String) {
-        roomRepository.deleteRoomBooking(roomBookingID)
-
+    fun deleteRoomBooking(roomBookingID: String, onSuccess: (Boolean) -> Unit) {
+        roomRepository.deleteRoomBooking(roomBookingID) {
+            onSuccess(it)
+        }
     }
 
     class RoomViewModelFactory(private val repository: RoomRepository) :
