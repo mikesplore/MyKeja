a6eb1b1701d25396d1e6c5089ab9851e1b051d60
mikesplore <mikepremium8@gmail.com>
2024-09-19 13:49:40 +0300

    Backend Implementation

--- app/src/main/java/com/mike/hms/MainActivity.kt
+++ app/src/main/java/com/mike/hms/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.mike.hms
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import com.mike.hms.ui.theme.HostelManagementSystemTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HostelManagementSystemTheme(darkTheme = false) {
+            HostelManagementSystemTheme(darkTheme = false, dynamicColor = false) {
 
             }
         }
