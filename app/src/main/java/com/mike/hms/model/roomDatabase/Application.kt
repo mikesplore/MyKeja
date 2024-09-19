757f8b952210fda7a57918a0a9af2a3ce26b49ed
mikesplore <mikepremium8@gmail.com>
2024-09-19 13:49:56 +0300

    Backend Implementation

---
+++ app/src/main/java/com/mike/hms/model/roomDatabase/Application.kt
@@ -0,0 +1,16 @@
+package com.mike.hms.model.roomDatabase
+
+import android.app.Application
+import com.mike.hms.model.bedModel.BedRepository
+import com.mike.hms.model.houseModel.HouseRepository
+import com.mike.hms.model.roomModel.RoomRepository
+import com.mike.hms.model.tenantModel.TenantRepository
+
+class HmsAPP () : Application() {
+    val database by lazy { HMSDatabase.getDatabase(this) }
+    val tenantRepository by lazy { TenantRepository(database.tenantDao()) }
+    val houseRepository by lazy { HouseRepository(database.houseDao()) }
+    val roomRepository by lazy { RoomRepository(database.roomDao()) }
+    val bedRepository by lazy { BedRepository(database.bedDao()) }
+
+}
