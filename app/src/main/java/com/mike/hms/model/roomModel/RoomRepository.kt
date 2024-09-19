dee097d2131f248170ccde77067e7c52791bf7fb
mikesplore <mikepremium8@gmail.com>
2024-09-19 18:42:40 +0300

    Backend Implementation

--- app/src/main/java/com/mike/hms/model/roomModel/RoomRepository.kt
+++ app/src/main/java/com/mike/hms/model/roomModel/RoomRepository.kt
@@ -1,16 +1,26 @@
 package com.mike.hms.model.roomModel
 
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class RoomRepository(private val roomDao: RoomDao) {
     private val viewmodelScope = CoroutineScope(Dispatchers.IO)
+    private val database = FirebaseDatabase.getInstance().reference
 
     fun insertRoom(room: RoomEntity, onSuccess: (Boolean) -> Unit) {
         viewmodelScope.launch {
             roomDao.insertRoom(room)
             onSuccess(true)
+        }
+
+        insertRoomToFirebase(room) { success ->
+            if (success) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
         }
     }
 
@@ -25,7 +35,14 @@ class RoomRepository(private val roomDao: RoomDao) {
         viewmodelScope.launch {
             val rooms = roomDao.getAllRooms()
             onResult(rooms)
-
+        }
+        retrieveRoomsFromFirebase { roomList ->
+            viewmodelScope.launch {
+                roomList.forEach { room ->
+                    roomDao.insertRoom(room)
+                }
+                onResult(roomList)
+            }
         }
     }
 
@@ -34,12 +51,26 @@ class RoomRepository(private val roomDao: RoomDao) {
             roomDao.deleteRoom(roomID)
             onSuccess(true)
         }
+        deleteRoomFromFirebase(roomID) { success ->
+            if (success) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
+        }
     }
 
     fun insertRoomAllocation(roomAllocation: RoomAllocationEntity, onSuccess: (Boolean) -> Unit) {
         viewmodelScope.launch {
             roomDao.insertRoomAllocation(roomAllocation)
             onSuccess(true)
+        }
+        insertRoomAllocationToFirebase(roomAllocation) { success ->
+            if (success) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
         }
     }
 
@@ -55,6 +86,14 @@ class RoomRepository(private val roomDao: RoomDao) {
             val roomAllocations = roomDao.getAllRoomAllocations()
             onResult(roomAllocations)
         }
+        retrieveRoomAllocationFromFirebase { roomAllocationList ->
+            viewmodelScope.launch {
+                roomAllocationList.forEach { roomAllocation ->
+                    roomDao.insertRoomAllocation(roomAllocation)
+                }
+                onResult(roomAllocationList)
+            }
+        }
     }
 
     fun deleteRoomAllocation(roomAllocationID: String, onSuccess: (Boolean) -> Unit) {
@@ -62,12 +101,26 @@ class RoomRepository(private val roomDao: RoomDao) {
             roomDao.deleteRoomAllocation(roomAllocationID)
             onSuccess(true)
         }
+        deleteRoomAllocationFromFirebase(roomAllocationID) { success ->
+            if (success) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
+        }
     }
 
     fun insertRoomBooking(roomBooking: RoomBookingEntity, onSuccess: (Boolean) -> Unit) {
         viewmodelScope.launch {
             roomDao.insertRoomBooking(roomBooking)
             onSuccess(true)
+        }
+        insertRoomBookingToFirebase(roomBooking) { success ->
+            if (success) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
         }
     }
 
@@ -78,9 +131,19 @@ class RoomRepository(private val roomDao: RoomDao) {
         }
     }
 
-    fun getAllRoomBookings(onResult: (List<RoomBookingEntity>) -> Unit) {
+    fun getAllRoomBookings() {
+        retrieveRoomBookingFromFirebase { roomBookingList ->
+            viewmodelScope.launch {
+                roomBookingList.forEach { roomBooking ->
+                    roomDao.insertRoomBooking(roomBooking)
+                }
+            }
+        }
+    }
+
+    fun getAllRoomBookingsWithTenantName(onResult: (List<RoomBookingWithTenantName>) -> Unit) {
         viewmodelScope.launch {
-            val roomBookings = roomDao.getAllRoomBookings()
+            val roomBookings = roomDao.getAllRoomBookingsWithTenantName()
             onResult(roomBookings)
         }
     }
@@ -89,6 +152,140 @@ class RoomRepository(private val roomDao: RoomDao) {
         viewmodelScope.launch {
             roomDao.deleteRoomBooking(roomBookingID)
             onSuccess(true)
+        }
+        deleteRoomBookingFromFirebase(roomBookingID) { success ->
+            if (success) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
+        }
+    }
+
+    //Firebase Functions
+
+    //Rooms
+    private fun insertRoomToFirebase(room: RoomEntity, onSuccess: (Boolean) -> Unit) {
+        val roomRef = database.child("Rooms").child(room.roomID)
+
+        roomRef.setValue(room).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
+        }
+    }
+
+    private fun retrieveRoomsFromFirebase(onSuccess: (List<RoomEntity>) -> Unit) {
+        val roomRef = database.child("Rooms")
+
+        roomRef.get().addOnSuccessListener { dataSnapshot ->
+            if (dataSnapshot.exists()) {
+                val roomList = mutableListOf<RoomEntity>()
+                for (roomSnapshot in dataSnapshot.children) {
+                    val room = roomSnapshot.getValue(RoomEntity::class.java)
+                    room?.let { roomList.add(it) }
+                }
+                onSuccess(roomList)
+            } else {
+                onSuccess(emptyList())
+            }
+        }
+    }
+
+    private fun deleteRoomFromFirebase(roomID: String, onSuccess: (Boolean) -> Unit) {
+        val roomRef = database.child("Rooms").child(roomID)
+        roomRef.removeValue().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
+        }
+    }
+
+    //Room Allocations
+    private fun insertRoomAllocationToFirebase(
+        room: RoomAllocationEntity,
+        onSuccess: (Boolean) -> Unit
+    ) {
+        val roomRef = database.child("RoomAllocation").child(room.roomID)
+
+        roomRef.setValue(room).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
+        }
+    }
+
+    private fun retrieveRoomAllocationFromFirebase(onSuccess: (List<RoomAllocationEntity>) -> Unit) {
+        val roomRef = database.child("RoomAllocation")
+
+        roomRef.get().addOnSuccessListener { dataSnapshot ->
+            if (dataSnapshot.exists()) {
+                val roomList = mutableListOf<RoomAllocationEntity>()
+                for (roomSnapshot in dataSnapshot.children) {
+                    val room = roomSnapshot.getValue(RoomAllocationEntity::class.java)
+                    room?.let { roomList.add(it) }
+                }
+                onSuccess(roomList)
+            } else {
+                onSuccess(emptyList())
+            }
+        }
+    }
+
+    private fun deleteRoomAllocationFromFirebase(roomID: String, onSuccess: (Boolean) -> Unit) {
+        val roomRef = database.child("RoomAllocation").child(roomID)
+        roomRef.removeValue().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
+        }
+    }
+
+    private fun insertRoomBookingToFirebase(room: RoomBookingEntity, onSuccess: (Boolean) -> Unit) {
+        val roomRef = database.child("RoomBooking").child(room.roomID)
+
+        roomRef.setValue(room).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
+        }
+    }
+
+    private fun retrieveRoomBookingFromFirebase(onSuccess: (List<RoomBookingEntity>) -> Unit) {
+        val roomRef = database.child("RoomBooking")
+
+        roomRef.get().addOnSuccessListener { dataSnapshot ->
+            if (dataSnapshot.exists()) {
+                val roomList = mutableListOf<RoomBookingEntity>()
+                for (roomSnapshot in dataSnapshot.children) {
+                    val room = roomSnapshot.getValue(RoomBookingEntity::class.java)
+                    room?.let { roomList.add(it) }
+                }
+                onSuccess(roomList)
+            } else {
+                onSuccess(emptyList())
+            }
+        }
+    }
+
+    private fun deleteRoomBookingFromFirebase(roomID: String, onSuccess: (Boolean) -> Unit) {
+        val roomRef = database.child("RoomBooking").child(roomID)
+        roomRef.removeValue().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
         }
     }
 }
