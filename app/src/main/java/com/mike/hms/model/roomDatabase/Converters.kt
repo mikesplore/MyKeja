e1e62705e1c78e926c333c0708c8d989312ad8a7
mikesplore <mikepremium8@gmail.com>
2024-09-19 13:50:08 +0300

    Backend Implementation

---
+++ app/src/main/java/com/mike/hms/model/roomDatabase/Converters.kt
@@ -0,0 +1,15 @@
+package com.mike.hms.model.roomDatabase
+
+import androidx.room.TypeConverter
+
+class Converters {
+    @TypeConverter
+    fun fromStringList(value: String?): List<String>? {
+        return value?.split(",")?.map { it.trim() }
+    }
+
+    @TypeConverter
+    fun toStringList(list: List<String>?): String? {
+        return list?.joinToString(",")
+    }
+}
