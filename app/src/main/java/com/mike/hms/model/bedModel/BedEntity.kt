7c706514000d4c693aea8934a7992b810889d97c
mikesplore <mikepremium8@gmail.com>
2024-09-19 14:56:14 +0300

    Backend Implementation

--- app/src/main/java/com/mike/hms/model/bedModel/BedEntity.kt
+++ app/src/main/java/com/mike/hms/model/bedModel/BedEntity.kt
@@ -1,10 +1,11 @@
 package com.mike.hms.model.bedModel
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "BedTable")
 class BedEntity(
-    val bedID: String,
+    @PrimaryKey val bedID: String,
     val bedType: String,
     val bedStatus: String,
     val roomID: String,
