22a935c80565717409e07a5d855e447d1318585e
mikesplore <mikepremium8@gmail.com>
2024-09-19 18:42:48 +0300

    Backend Implementation

--- app/src/main/java/com/mike/hms/model/tenantModel/TenantRepository.kt
+++ app/src/main/java/com/mike/hms/model/tenantModel/TenantRepository.kt
@@ -1,5 +1,6 @@
 package com.mike.hms.model.tenantModel
 
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -7,8 +8,17 @@ import kotlinx.coroutines.launch
 class TenantRepository(private val tenantDao: TenantDao) {
 
     private val viewmodelScope = CoroutineScope(Dispatchers.IO)
+    private val database = FirebaseDatabase.getInstance().reference
 
     fun insertTenant(tenant: TenantEntity, onSuccess: (Boolean) -> Unit) {
+
+        insertTenantToFirebase(tenant) { success ->
+            if (success) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
+        }
         viewmodelScope.launch {
             tenantDao.insertTenant(tenant)
             onSuccess(true)
@@ -27,6 +37,14 @@ class TenantRepository(private val tenantDao: TenantDao) {
             val tenants = tenantDao.getAllTenants()
             onResult(tenants)
         }
+        retrieveTenantsFromFirebase { tenantList ->
+            viewmodelScope.launch {
+                tenantList.forEach { tenant ->
+                    tenantDao.insertTenant(tenant)
+                }
+                onResult(tenantList)
+            }
+        }
     }
 
     fun deleteTenant(tenantID: String, onSuccess: (Boolean) -> Unit) {
@@ -34,6 +52,54 @@ class TenantRepository(private val tenantDao: TenantDao) {
             tenantDao.deleteTenant(tenantID)
             onSuccess(true)
 
+        }
+        deleteTenantFromFirebase(tenantID) { success ->
+            if (success) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
+        }
+    }
+
+    //Firebase Functions
+    private fun insertTenantToFirebase(tenant: TenantEntity, onSuccess: (Boolean) -> Unit) {
+        val tenantRef = database.child("Tenants").child(tenant.tenantID)
+
+        tenantRef.setValue(tenant).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
+        }
+    }
+
+    private fun retrieveTenantsFromFirebase(onSuccess: (List<TenantEntity>) -> Unit) {
+        val tenantRef = database.child("Tenants")
+
+        tenantRef.get().addOnSuccessListener { dataSnapshot ->
+            if (dataSnapshot.exists()) {
+                val tenantList = mutableListOf<TenantEntity>()
+                for (tenantSnapshot in dataSnapshot.children) {
+                    val tenant = tenantSnapshot.getValue(TenantEntity::class.java)
+                    tenant?.let { tenantList.add(it) }
+                }
+                onSuccess(tenantList)
+            } else {
+                onSuccess(emptyList())
+            }
+        }
+    }
+
+    private fun deleteTenantFromFirebase(tenantID: String, onSuccess: (Boolean) -> Unit) {
+        val tenantRef = database.child("Tenants").child(tenantID)
+        tenantRef.removeValue().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onSuccess(true)
+            } else {
+                onSuccess(false)
+            }
         }
     }
 }
