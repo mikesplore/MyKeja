afad8aeecf165cbdbcd8693a5a22dfc2b5f0b6ac
mikesplore <mikepremium8@gmail.com>
2024-09-19 14:56:29 +0300

    Backend Implementation

--- app/src/main/java/com/mike/hms/model/roomModel/RoomEntity.kt
+++ app/src/main/java/com/mike/hms/model/roomModel/RoomEntity.kt
@@ -1,22 +1,70 @@
 package com.mike.hms.model.roomModel
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "RoomTable")
 class RoomEntity(
-    val roomID: String,
+    @PrimaryKey val roomID: String,
     val roomNumber: String,
     val roomType: String,
     val roomCapacity: String,
+    val roomAvailability: String,
 ){
-    constructor() : this("", "", "", "")
+    constructor() : this("", "", "", "", "")
 
 }
 
+@Entity(tableName = "RoomAllocation")
+class RoomAllocationEntity(
+    @PrimaryKey val roomAllocationID: String,
+    val roomID: String,
+    val bedID: String,
+    val tenantID: String,
+    val checkInDate: String,
+    val checkOutDate: String,
+){
+    constructor() : this("", "","", "", "", "")
+}
+
+@Entity(tableName = "RoomBooking")
+class RoomBookingEntity(
+    @PrimaryKey val roomBookingID: String,
+    val roomID: String,
+    val tenantID: String,
+    val checkInDate: String,
+    val checkOutDate: String,
+    val bookingStatus: String,
+){
+    constructor() : this("", "","", "", "", "")
+}
+
+data class RoomBookingWithTenantName(
+    val roomBookingID: String,
+    val roomID: String,
+    val tenantID: String,
+    val checkInDate: String,
+    val checkOutDate: String,
+    val bookingStatus: String,
+    val tenantName: String // Added tenant name
+){
+    constructor() : this("", "","", "", "", "", "")
+}
 
 enum class RoomType{
     PRIVATE,
     SHARED
+}
+
+enum class BookingStatus{
+    PENDING,
+    CONFIRMED,
+    CANCELLED
+}
+
+enum class RoomAvailability{
+    AVAILABLE,
+    UNAVAILABLE
 }
 
 
