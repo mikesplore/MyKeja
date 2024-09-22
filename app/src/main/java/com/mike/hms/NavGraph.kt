df0bc59135860fb00f4562bd22928402be695c0a
mikesplore <mikepremium8@gmail.com>
2024-09-23 00:21:14 +0300

    Continuation

--- app/src/main/java/com/mike/hms/NavGraph.kt
+++ app/src/main/java/com/mike/hms/NavGraph.kt
@@ -6,14 +6,19 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.mike.hms.dashboard.DashboardScreen
+import com.mike.hms.homeScreen.HomeScreen
 
 @Composable
 fun NavGraph(context: Context){
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "dashboard"){
+    NavHost(navController = navController, startDestination = "homeScreen"){
         composable("dashboard"){
             DashboardScreen(context, navController)
+        }
+
+        composable("homeScreen"){
+            HomeScreen(navController, context)
         }
     }
 
