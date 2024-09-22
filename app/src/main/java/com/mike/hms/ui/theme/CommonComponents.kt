dad0a87c731c2ae151fc2f0eb8a41afcd80151d6
mikesplore <mikepremium8@gmail.com>
2024-09-22 17:55:14 +0300

    Backend Implementation

--- app/src/main/java/com/mike/hms/ui/theme/CommonComponents.kt
+++ app/src/main/java/com/mike/hms/ui/theme/CommonComponents.kt
@@ -1,13 +1,26 @@
 package com.mike.hms.ui.theme
 
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 object CommonComponents{
@@ -20,6 +33,16 @@ object CommonComponents{
     @Composable
     fun secondaryColor(): Color{
         return MaterialTheme.colorScheme.secondary
+    }
+
+    @Composable
+    fun extraPrimaryColor(): Color{
+        return MaterialTheme.colorScheme.onPrimary
+    }
+
+    @Composable
+    fun extraSecondaryColor(): Color{
+        return MaterialTheme.colorScheme.onSecondary
     }
 
     @Composable
@@ -88,4 +111,57 @@ object CommonComponents{
             colors = OutlinedTextFieldDefaults.colors()
         )
     }
+
+    //Search TextField
+    @Composable
+    fun SearchTextField(
+        modifier: Modifier = Modifier,
+        value: String,
+        onValueChange: (String) -> Unit,
+        placeholder: String
+    ) {
+        OutlinedTextField(
+            value = value,
+            textStyle = contentTextStyle(),
+            onValueChange = { onValueChange(it) }, // Use 'it' to get the updated value
+            singleLine = true,
+            placeholder = { Text(placeholder, style = contentTextStyle()) },
+            shape = RoundedCornerShape(20.dp),
+            modifier = modifier,
+            colors = OutlinedTextFieldDefaults.colors(),
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "Search Icon"
+                )
+            }
+        )
+    }
+
+    @Composable
+    fun AdaptiveSizes(
+        modifier: Modifier = Modifier,
+        textFraction: Float = 1.0f, // Fraction for text size
+        dpFraction: Float = 1.0f,   // Fraction for DP size
+        content: @Composable (textSize: TextUnit, dpSize: Dp) -> Unit
+    ) {
+        BoxWithConstraints(modifier = modifier) {
+            val columnWidth = maxWidth
+            val density = LocalDensity.current
+            val textSize = with(density) { (columnWidth * textFraction).toSp() }
+            val dpSize = columnWidth * dpFraction
+            content(textSize, dpSize)
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun topAppBarColors(): TopAppBarColors =
+        TopAppBarDefaults.topAppBarColors(
+            containerColor = primaryColor(),
+            titleContentColor = textColor(),
+            navigationIconContentColor = textColor(),
+            actionIconContentColor = textColor(),
+            scrolledContainerColor = primaryColor()
+        )
 }
