cf1fc29b1b8c2e3eae26188d014bfa5a2617be4e
mikesplore <mikepremium8@gmail.com>
2024-09-22 14:34:49 +0300

    Backend Implementation

--- app/src/main/java/com/mike/hms/model/roomDatabase/Converters.kt
+++ app/src/main/java/com/mike/hms/model/roomDatabase/Converters.kt
@@ -1,6 +1,7 @@
 package com.mike.hms.model.roomDatabase
 
 import androidx.room.TypeConverter
+import com.mike.hms.model.roomModel.AmenityType
 
 class Converters {
     @TypeConverter
@@ -11,5 +12,17 @@ class Converters {
     @TypeConverter
     fun toStringList(list: List<String>?): String? {
         return list?.joinToString(",")
+    }
+
+    // Convert Set<AmenityType> to a comma-separated string for storage
+    @TypeConverter
+    fun fromSet(amenities: Set<AmenityType>): String {
+        return amenities.joinToString(separator = ",") { it.name }
+    }
+
+    // Convert a comma-separated string back into a Set<AmenityType>
+    @TypeConverter
+    fun toSet(amenitiesString: String): Set<AmenityType> {
+        return amenitiesString.split(",").map { AmenityType.valueOf(it) }.toSet()
     }
 }
