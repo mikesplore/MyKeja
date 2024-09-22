451af013a3997359edee77ac304efcc7ca0a9e1b
mikesplore <mikepremium8@gmail.com>
2024-09-22 17:55:38 +0300

    Continuation

---
+++ app/src/main/java/com/mike/hms/homeScreen/MockData.kt
@@ -0,0 +1,16 @@
+package com.mike.hms.homeScreen
+
+import com.mike.hms.model.houseModel.HouseEntity
+import com.mike.hms.model.houseModel.HouseType
+
+// House types
+val houseTypes  = listOf(
+    HouseType(houseTypeID = "1", houseType = "Apartment", houseTypeImageLink = ""),
+    HouseType(houseTypeID = "2", houseType = "Condo", houseTypeImageLink = ""),
+    HouseType(houseTypeID = "3", houseType = "House", houseTypeImageLink = ""),
+    HouseType(houseTypeID = "4", houseType = "Townhouse", houseTypeImageLink = ""),
+    HouseType(houseTypeID = "5", houseType = "Villa", houseTypeImageLink = ""),
+    HouseType(houseTypeID = "6", houseType = "Other", houseTypeImageLink = ""),
+
+
+)
