101983352dfc9f702a7fb64429e2fcd6417f3cc0
mikesplore <mikepremium8@gmail.com>
2024-09-23 00:21:28 +0300

    Continuation

--- app/src/main/java/com/mike/hms/ui/theme/Color.kt
+++ app/src/main/java/com/mike/hms/ui/theme/Color.kt
@@ -2,11 +2,13 @@ package com.mike.hms.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Black = Color(0xFF000000)
+val Black = Color(0xFF212A3E)
 val White = Color(0xFFffffff)
 val Blue = Color (0XFF3FA2F6)
 val Red = Color(0XFFD20062)
 val WhiteSchemeTextColor = Color(0xff526D82)
 val BlackSchemeTextColor = Color(0xFF9DB2BF)
-val Green = Color(0xFF00FF00)
-val Purple = Color(0XFF4E31AA)
+val Green = Color(0xFF76B39D)
+val Purple = Color(0XFF615EFC)
+val whiteSurfaceContainer = Black
+val blackSurfaceContainer = BlackSchemeTextColor
