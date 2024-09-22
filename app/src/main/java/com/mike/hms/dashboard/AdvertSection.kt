8e8f60ddaaf872af409506a7e0fd9431fc064602
mikesplore <mikepremium8@gmail.com>
2024-09-23 00:21:00 +0300

    Continuation

--- app/src/main/java/com/mike/hms/dashboard/AdvertSection.kt
+++ app/src/main/java/com/mike/hms/dashboard/AdvertSection.kt
@@ -1,18 +1,23 @@
 package com.mike.hms.dashboard
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -37,6 +42,10 @@ fun CarouselItemCard(carouselItem: CarouselItem) {
                     color = CC.secondaryColor(),
                     shape = RoundedCornerShape(20.dp)
                 )
+                .shadow(
+                    elevation = 10.dp,
+                    shape = RoundedCornerShape(20.dp)
+                )
 
         ) {
             AsyncImage(
@@ -46,7 +55,7 @@ fun CarouselItemCard(carouselItem: CarouselItem) {
                     .fillMaxSize(),
                 contentScale = ContentScale.Crop,
 
-            )
+                )
 
         }
     }
