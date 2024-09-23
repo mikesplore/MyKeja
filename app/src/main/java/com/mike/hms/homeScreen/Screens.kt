418cf9bbb9c9c59c669533a2d3ca23f9fa3a7662
mikesplore <mikepremium8@gmail.com>
2024-09-23 12:44:46 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/homeScreen/Screens.kt
+++ app/src/main/java/com/mike/hms/homeScreen/Screens.kt
@@ -1,12 +1,16 @@
 package com.mike.hms.homeScreen
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Chat
+import androidx.compose.material.icons.automirrored.outlined.Chat
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Chat
 import androidx.compose.material.icons.filled.ChatBubble
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Payment
 import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Chat
 import androidx.compose.material.icons.outlined.ChatBubble
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.Home
@@ -29,7 +33,7 @@ sealed class Screen(
     )
 
     data object Chat : Screen(
-        Icons.Filled.ChatBubble, Icons.Outlined.ChatBubble, "Chat"
+        Icons.AutoMirrored.Filled.Chat, Icons.AutoMirrored.Outlined.Chat, "Chat"
     )
 
     data object Profile : Screen(
