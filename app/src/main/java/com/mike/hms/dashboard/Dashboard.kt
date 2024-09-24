81d03b215fbafe266012621eb850e2027c8073e8
mikesplore <mikepremium8@gmail.com>
2024-09-24 14:24:18 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/dashboard/Dashboard.kt
+++ app/src/main/java/com/mike/hms/dashboard/Dashboard.kt
@@ -1,7 +1,10 @@
 package com.mike.hms.dashboard
 
 import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,13 +13,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.mike.hms.homeScreen.TopAppBarComponent
+import com.mike.hms.model.roomModel.RoomCategory
 import com.mike.hms.ui.theme.CommonComponents as CC
 
 @Composable
@@ -29,6 +39,7 @@ fun DashboardScreen(context: Context, navController: NavController) {
     ) { innerPadding ->
         Column(
             modifier = Modifier
+                .verticalScroll(rememberScrollState())
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
@@ -45,6 +56,16 @@ fun DashboardScreen(context: Context, navController: NavController) {
             Spacer(modifier = Modifier.height(30.dp))
             HouseTypeList()
             Spacer(modifier = Modifier.height(20.dp))
+            RoomsCategory()
+            Spacer(modifier = Modifier.height(20.dp))
+            RecommendedRoomTypeList()
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                "Offers for you", style = CC.titleTextStyle().copy(
+                    color = CC.secondaryColor()
+                ),
+                modifier = Modifier.padding(start = 20.dp)
+            )
             CarouselWithLoop()
             Spacer(modifier = Modifier.height(20.dp))
             Row(
@@ -82,15 +103,13 @@ fun DashboardScreen(context: Context, navController: NavController) {
                 Text(
                     "See all", style = CC.contentTextStyle().copy(
                         color = CC.extraPrimaryColor()
-                    ),
-
-                    )
-
+                    ))
             }
+            Spacer(modifier = Modifier.height(30.dp))
+
         }
     }
 }
-
 
 
 @Composable
