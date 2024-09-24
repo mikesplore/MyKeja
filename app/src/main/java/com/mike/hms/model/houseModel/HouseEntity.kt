8075d1b17cc5a8b74ba59f2c894602e4a60b682b
mikesplore <mikepremium8@gmail.com>
2024-09-24 17:18:03 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/model/houseModel/HouseEntity.kt
+++ app/src/main/java/com/mike/hms/model/houseModel/HouseEntity.kt
@@ -2,26 +2,31 @@ package com.mike.hms.model.houseModel
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.mike.hms.houses.HouseTypes
 import com.mike.hms.model.roomModel.RoomEntity
 
 @Entity(tableName = "HouseTable")
 data class HouseEntity(
     @PrimaryKey val houseID: String,
-    val houseName: String = "",
-    val houseType: String = "",                  // Type of house (e.g., villa, apartment)
+    val houseName: String = "" ,
+    val houseType: HouseType = HouseType.HOTEL,                  // Type of house (e.g., villa, apartment)
     val houseLocation: String = "",              // General location or address
     val houseRating: String = "5.0",        // Rating of the house
-    val houseImageLink: String = "",             // Image link for the house
+    val houseImageLink: List<String> = emptyList(),             // Image link for the house
     val houseDescription: String = "",           // Description of the house
     val ownerID: String = "",                    // ID of the house owner: HouseOwner,                  // Link to the owner data class
     val bookingInfoID: String = "",           // Link to the booking information
     val rooms: List<String>   = listOf()                    // List of rooms id available in the house
-)
+){
+    constructor() : this("","",  HouseType.HOTEL, "", "", emptyList(), "", "", "", emptyList())
+}
 
 data class HouseOwner(
     val ownerName: String,                  // Name of the house owner
     val ownerContact: String                // Contact info (email or phone)
-)
+){
+    constructor() : this("", "")
+}
 
 data class BookingInfo(
     val bookingID: String,                  // ID of the booking
@@ -29,7 +34,18 @@ data class BookingInfo(
     val houseAvailable: Boolean,            // Availability status (true/false)
     val visitCount: Int = 0,                // Number of visits or views for the house
     val bookingPolicy: String               // Booking or cancellation policy
-)
+){
+    constructor() : this("", "", false, 0, "")
+}
+
+enum class HouseType {
+    VILLA,
+    APARTMENT,
+    HOTEL,
+    BUNGALOW,
+    CONDOMINIUM,
+    BOUTIQUES
+}
 
 
 
