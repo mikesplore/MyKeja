f7d9ae7746a90679c5af15571bb100cdce29b1b0
mikesplore <mikepremium8@gmail.com>
2024-09-24 21:35:07 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/houses/Amenities.kt
+++ app/src/main/java/com/mike/hms/houses/Amenities.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -32,54 +35,59 @@ fun HouseAmenities(house: HouseEntity) {
     val screenHeight = configuration.screenHeightDp.dp
 
     val brush = Brush.horizontalGradient(
-        listOf(CC.primaryColor(), CC.secondaryColor().copy(alpha = 0.5f))
+        listOf(CC.surfaceContainerColor(), CC.secondaryColor().copy(alpha = 0.5f))
     )
 
-    Column(
+    Card(
         modifier = Modifier
-            .background(
-                brush, RoundedCornerShape(10.dp)
-            )
             .padding(bottom = 10.dp)
             .fillMaxWidth(0.9f)
-            .heightIn(max = screenHeight * 0.2f)
+            .heightIn(max = screenHeight * 0.2f),
+        elevation = CardDefaults.cardElevation(4.dp),
     ) {
-        Text(
-            "Amenities", style = CC.titleTextStyle().copy(
-                color = CC.tertiaryColor()
-            ),
-            fontWeight = FontWeight.Bold,
-            modifier =
-            Modifier.padding(16.dp)
-        )
-
-        FlowRow(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-
-
+                .background(brush)
+                .fillMaxSize()
         ) {
-            amenities.forEach { amenity ->
-                Box(
-                    modifier = Modifier
-                        .border(
-                            width = 1.dp,
-                            color = CC.primaryColor(),
-                            shape = RoundedCornerShape(10.dp)
+            Text(
+                "Amenities", style = CC.titleTextStyle().copy(
+                    color = CC.tertiaryColor()
+                ),
+                fontWeight = FontWeight.Bold,
+                modifier =
+                Modifier.padding(16.dp)
+            )
+
+            FlowRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+
+
+            ) {
+                amenities.forEach { amenity ->
+                    Box(
+                        modifier = Modifier
+                            .border(
+                                width = 1.dp,
+                                color = CC.primaryColor(),
+                                shape = RoundedCornerShape(10.dp)
+                            )
+                            .padding(8.dp)
+                    ) {
+                        Text(
+                            text = amenity.first().uppercase() + amenity.substring(1).lowercase()
+                                .replace("_", " "),
+                            style = CC.contentTextStyle().copy(
+                                fontSize = 14.sp,
+                                color = CC.textColor(),
+                                textAlign = TextAlign.Center
+                            )
                         )
-                        .padding(8.dp)
-                ) {
-                    Text(
-                        text = amenity.first().uppercase() + amenity.substring(1).lowercase().replace("_", " "),
-                        style = CC.contentTextStyle().copy(
-                            fontSize = 14.sp,
-                            color = CC.textColor(),
-                            textAlign = TextAlign.Center
-                        )
-                    )
+                    }
                 }
             }
         }
