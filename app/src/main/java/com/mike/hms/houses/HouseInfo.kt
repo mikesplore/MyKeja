5ec19eb0ff9e58810b9bfb111e5bf8c67eefca82
mikesplore <mikepremium8@gmail.com>
2024-09-24 19:05:25 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/houses/HouseInfo.kt
+++ app/src/main/java/com/mike/hms/houses/HouseInfo.kt
@@ -1,14 +1,17 @@
 package com.mike.hms.houses
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -17,13 +20,14 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,14 +35,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import coil.compose.AsyncImage
 import com.mike.hms.homeScreen.houseTypes
-import java.util.Locale
 import com.mike.hms.ui.theme.CommonComponents as CC
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,95 +54,100 @@ fun HouseDetailScreen(
     val configuration = LocalConfiguration.current
     val screenWidth = configuration.screenWidthDp.dp
     val screenHeight = configuration.screenHeightDp.dp
+    val density = LocalDensity.current.density
+    val textSize = (screenWidth.value / density).sp
+    val isHouseFavorite = remember { mutableStateOf(false) }
 
-    val house = houseTypes[0]
+
+    val house = houseTypes.random()
     var selectedImage by remember { mutableStateOf<String?>(null) }
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(house.houseName, style = CC.titleTextStyle()) },
-                navigationIcon = {
-                    IconButton(onClick = { }) {
-                        Icon(Icons.Default.ArrowBackIosNew, contentDescription = "Back")
-                    }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = CC.primaryColor(),
-                    titleContentColor = CC.primaryColor(),
-                    navigationIconContentColor = CC.secondaryColor()
-                )
-            )
-        }
-    ) { innerPadding ->
-        Column(
+    Column(
+        modifier = Modifier
+            .background(CC.primaryColor())
+            .padding(top = 16.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        // Image Gallery
+        LazyRow(
             modifier = Modifier
-                .padding(innerPadding)
-                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(vertical = 8.dp)
         ) {
-            // Image Gallery
-            LazyRow(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-                    .padding(vertical = 8.dp)
-            ) {
-                items(house.houseImageLink) { imageUrl ->
+            items(house.houseImageLink) { imageUrl ->
+                Box(modifier = Modifier
+                    .height(screenHeight)
+                    .width(screenWidth)){
                     AsyncImage(
                         model = imageUrl,
                         contentDescription = "House Image",
                         modifier = Modifier
                             .height(screenHeight * 0.3f)
                             .width(screenWidth)
-                            .padding(horizontal = 4.dp)
                             .clickable { selectedImage = imageUrl },
                         contentScale = ContentScale.Crop
                     )
-                }
-            }
+                    IconButton(onClick = {},
+                        colors = IconButtonDefaults.iconButtonColors(
+                            containerColor = CC.primaryColor()
+                        ),
+                        modifier = Modifier
+                            .padding(start = 5.dp)
+                            .align(Alignment.TopStart)) {
+                        Icon(
+                            Icons.Default.ArrowBackIosNew,
+                            contentDescription = "Close",
+                            tint = CC.tertiaryColor()
+                        )
+                    }
 
-            // House Details
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(
-                    "Type: ${house.houseType.name.replaceFirstChar {
-                        if (it.isLowerCase()) it.titlecase(
-                            Locale.getDefault()
-                        ) else it.toString()
-                    }}",
-                    style = CC.bodyTextStyle()
-                )
-                Text(
-                    "Location: ${house.houseLocation}",
-                    style = CC.contentTextStyle()
-                )
-                Text(
-                    "Rating: ${house.houseRating}",
-                    style = CC.contentTextStyle()
-                )
-                Text(
-                    "Price: $${house.housePrice}",
-                    style = CC.contentTextStyle()
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    "Description:",
-                    style = CC.bodyTextStyle()
-                )
-                Text(
-                    house.houseDescription,
-                    style = CC.contentTextStyle()
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    "Available Rooms:",
-                    style = CC.bodyTextStyle()
-                )
-                house.rooms.forEach { room ->
-                    Text("• $room", style = CC.contentTextStyle())
+                    IconButton(
+                        onClick = {
+                            isHouseFavorite.value = !isHouseFavorite.value
+                        },
+                        colors = IconButtonDefaults.iconButtonColors(
+                            containerColor = CC.primaryColor()
+                        ),
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)) {
+                        Icon(
+                            if (isHouseFavorite.value) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                            contentDescription = "Favorite",
+                            tint = CC.tertiaryColor(),
+                            modifier = Modifier.size(screenWidth * 0.05f)
+                        )
+                    }
+                    IconButton(onClick = { },
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd),
+                        colors = IconButtonDefaults.iconButtonColors(
+                            containerColor = CC.primaryColor()
+                        )) {
+                        Icon(
+                            Icons.Default.Share,
+                            contentDescription = "Share",
+                            tint = CC.tertiaryColor(),
+                            modifier = Modifier.size(screenWidth * 0.05f)
+                        )
+                    }
                 }
             }
         }
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = house.houseName,
+                style = CC.titleTextStyle().copy(fontSize = textSize * 0.15f)
+            )
+        }
     }
+
 
     // Full-screen image dialog
     selectedImage?.let { imageUrl ->
