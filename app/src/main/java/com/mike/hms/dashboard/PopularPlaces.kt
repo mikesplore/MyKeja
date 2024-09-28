26b971cd0a3f089bcd36ba72334f6528afa52929
mikesplore <mikepremium8@gmail.com>
2024-09-28 22:08:58 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/dashboard/PopularPlaces.kt
+++ app/src/main/java/com/mike/hms/dashboard/PopularPlaces.kt
@@ -1,10 +1,8 @@
 package com.mike.hms.dashboard
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -27,11 +25,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.mike.hms.homeScreen.houseTypes
@@ -40,78 +36,89 @@ import java.util.Locale
 import com.mike.hms.ui.theme.CommonComponents as CC
 
 
-
 @Composable
 fun PopularHouseItem(houseType: HouseEntity, modifier: Modifier = Modifier) {
-        val configuration = LocalConfiguration.current
-        val screenWidth = configuration.screenWidthDp.dp
-        val boxWidth = screenWidth * 0.35f
-        val boxHeight = boxWidth * 1.3f
-        val density = LocalDensity.current
-        val textSize = with(density) { (boxHeight * 0.1f).toSp() }
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val boxWidth = screenWidth * 0.35f
+    val boxHeight = boxWidth * 1.3f
+    val density = LocalDensity.current
+    val textSize = with(density) { (boxHeight * 0.1f).toSp() }
 
-        // Card for the house item
-        Card(
-            modifier = modifier
-                .width(boxWidth)
-                .height(boxHeight),
-            shape = RoundedCornerShape(20.dp),  // Rounded corners
-            elevation = CardDefaults.cardElevation(5.dp)                 // Built-in shadow/elevation
-        ) {
-            Box {
-                // Image
-                AsyncImage(
-                    model = houseType.houseImageLink.random(),
-                    contentDescription = "House Image",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(RoundedCornerShape(20.dp)),
-                    contentScale = ContentScale.Crop
+    // Card for the house item
+    Card(
+        modifier = modifier
+            .width(boxWidth)
+            .height(boxHeight),
+        shape = RoundedCornerShape(20.dp),  // Rounded corners
+        elevation = CardDefaults.cardElevation(5.dp)                 // Built-in shadow/elevation
+    ) {
+        Box {
+            // Image
+            AsyncImage(
+                model = houseType.houseImageLink.random(),
+                contentDescription = "House Image",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(20.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            // Bottom overlay with house name, location, and rating
+            Column(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .background(
+                        CC
+                            .surfaceContainerColor()
+                            .copy(alpha = 0.9f)
+                    )
+                    .padding(5.dp)  // Padding for a cleaner layout
+            ) {
+                // House name, type and location
+                Text(
+                    text = "${houseType.houseName}, ${
+                        if (houseType.houseType.name.startsWith(
+                                "a",
+                                ignoreCase = true
+                            )
+                        ) "an" else "a"
+                    } ${
+                        houseType.houseType.name.lowercase(
+                            Locale.getDefault()
+                        )
+                    } in ${houseType.houseLocation}",
+                    style = CC.bodyTextStyle().copy(
+                        fontSize = textSize * 0.6f,
+                        color = CC.tertiaryColor()
+                    ),
+                    modifier = Modifier.padding(bottom = 5.dp)
                 )
 
-                // Bottom overlay with house name, location, and rating
-                Column(
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter)
-                        .fillMaxWidth()
-                        .background(CC.surfaceContainerColor().copy(alpha = 0.9f))
-                        .padding(5.dp)  // Padding for a cleaner layout
+                // Row for rating with star icon
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,  // Center the star and rating
+                    modifier = Modifier.padding(start = 4.dp)  // Padding for structure
                 ) {
-                    // House name, type and location
-                    Text(
-                        text = "${houseType.houseName}, ${if (houseType.houseType.name.startsWith("a", ignoreCase = true)) "an" else "a"} ${houseType.houseType.name.lowercase(
-                            Locale.getDefault()
-                        )} in ${houseType.houseLocation}",
-                        style = CC.bodyTextStyle().copy(
-                            fontSize = textSize * 0.6f,
-                            color = CC.tertiaryColor()
-                        ),
-                        modifier = Modifier.padding(bottom = 5.dp)
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "Star Icon",
+                        tint = CC.extraPrimaryColor(),
+                        modifier = Modifier.size(20.dp)
                     )
-
-                    // Row for rating with star icon
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,  // Center the star and rating
-                        modifier = Modifier.padding(start = 4.dp)  // Padding for structure
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Star,
-                            contentDescription = "Star Icon",
-                            tint = CC.extraPrimaryColor(),
-                            modifier = Modifier.size(20.dp)
+                    Spacer(modifier = Modifier.width(4.dp))  // Space between star and rating
+                    Text(
+                        text = houseType.houseRating,
+                        style = CC.bodyTextStyle().copy(
+                            fontSize = textSize * 0.7f,
+                            color = CC.extraPrimaryColor()
                         )
-                        Spacer(modifier = Modifier.width(4.dp))  // Space between star and rating
-                        Text(
-                            text = houseType.houseRating,
-                            style = CC.bodyTextStyle().copy(
-                                fontSize = textSize * 0.7f,
-                                color = CC.extraPrimaryColor()
-                            )
-                        )
-                    }
+                    )
                 }
             }
         }
+    }
 
 }
 
