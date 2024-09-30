89f08549be3380dbc9fa132e021b4220a961bb58
mikesplore <mikepremium8@gmail.com>
2024-09-30 13:45:57 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/model/roomDatabase/Converters.kt
+++ app/src/main/java/com/mike/hms/model/roomDatabase/Converters.kt
@@ -1,11 +1,9 @@
 package com.mike.hms.model.roomDatabase
 
-import androidx.compose.ui.input.key.type
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.mike.hms.model.houseModel.HouseAmenities
-import com.mike.hms.model.roomModel.AmenityType
 
 class Converters {
     @TypeConverter
@@ -16,18 +14,6 @@ class Converters {
     @TypeConverter
     fun toStringList(list: List<String>?): String? {
         return list?.joinToString(",")
-    }
-
-    // Convert Set<AmenityType> to a comma-separated string for storage
-    @TypeConverter
-    fun fromSet(amenities: Set<AmenityType>): String {
-        return amenities.joinToString(separator = ",") { it.name }
-    }
-
-    // Convert a comma-separated string back into a Set<AmenityType>
-    @TypeConverter
-    fun toSet(amenitiesString: String): Set<AmenityType> {
-        return amenitiesString.split(",").map { AmenityType.valueOf(it) }.toSet()
     }
 
     @TypeConverter
