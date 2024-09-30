72ebb487c9649eeeb6a2848f7ca7e88e64230325
mikesplore <mikepremium8@gmail.com>
2024-09-30 13:56:08 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/model/tenantModel/TenantEntity.kt
+++ app/src/main/java/com/mike/hms/model/tenantModel/TenantEntity.kt
@@ -1,11 +1,11 @@
-package com.mike.hms.model.tenantModel
+package com.mike.hms.model.userModel
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "tenantTable")
-data class TenantEntity(
-    @PrimaryKey val tenantID: String,
+@Entity(tableName = "userTable")
+data class UserEntity(
+    @PrimaryKey val userID: String,
     val firstName: String,
     val lastName: String,
     val gender: String,
