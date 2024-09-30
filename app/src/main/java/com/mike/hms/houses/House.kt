2e82afae4158ffe1285567d1e8e45f2a104259be
mikesplore <mikepremium8@gmail.com>
2024-09-30 20:01:18 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/houses/House.kt
+++ app/src/main/java/com/mike/hms/houses/House.kt
@@ -2,49 +2,26 @@ package com.mike.hms.houses
 
 import android.content.Context
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.FilterChipDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.mike.hms.model.getHouseViewModel
 import com.mike.hms.model.houseModel.HouseEntity
@@ -215,39 +192,40 @@ fun BookNow(house: HouseEntity) {
             containerColor = CC.secondaryColor()
         ),
     ) {
-    Column(
-        modifier = Modifier
-            .padding(horizontal = 16.dp, vertical = 12.dp), // Adjusted padding for more space
-        horizontalAlignment = Alignment.CenterHorizontally // Center content horizontally
-    ) {
-        Text(
-            text = "Ksh ${formatNumber(house.housePrice)} / night",
-            style = CC.titleTextStyle().copy(
-                fontSize = textSize * 0.7f,
-                color = CC.extraSecondaryColor()
-            )
-        )
-
-        Button(
-            onClick = { /* Handle book now action */ },
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp), // Add space above the button
-            colors = ButtonDefaults.buttonColors(
-                containerColor = CC.primaryColor(), // Set button background color
-                contentColor = Color.White // Set button text color
-            ),
-            shape = RoundedCornerShape(10.dp) // Rounded corners for the button
+                .padding(horizontal = 16.dp, vertical = 12.dp), // Adjusted padding for more space
+            horizontalAlignment = Alignment.CenterHorizontally // Center content horizontally
         ) {
             Text(
-                text = "Book Now",
-                style = CC.contentTextStyle().copy(
-                    fontSize = textSize * 0.7f, // Adjust font size for the button
-                    fontWeight = FontWeight.Bold // Make button text bold
+                text = "Ksh ${formatNumber(house.housePrice)} / night",
+                style = CC.titleTextStyle().copy(
+                    fontSize = textSize * 0.7f,
+                    color = CC.extraSecondaryColor()
                 )
             )
+
+            Button(
+                onClick = { /* Handle book now action */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp), // Add space above the button
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = CC.primaryColor(), // Set button background color
+                    contentColor = Color.White // Set button text color
+                ),
+                shape = RoundedCornerShape(10.dp) // Rounded corners for the button
+            ) {
+                Text(
+                    text = "Book Now",
+                    style = CC.contentTextStyle().copy(
+                        fontSize = textSize * 0.7f, // Adjust font size for the button
+                        fontWeight = FontWeight.Bold // Make button text bold
+                    )
+                )
+            }
         }
-    }}
+    }
 }
 
 
