bd19da7c48353fc12cb2cd53e16c1e8ea6bc4422
mikesplore <mikepremium8@gmail.com>
2024-09-30 20:01:00 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/model/userModel/UserEntity.kt
+++ app/src/main/java/com/mike/hms/model/userModel/UserEntity.kt
@@ -3,7 +3,7 @@ package com.mike.hms.model.userModel
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "userTable")
+@Entity(tableName = "users")
 data class UserEntity(
     @PrimaryKey val userID: String,
     val firstName: String,
@@ -11,8 +11,9 @@ data class UserEntity(
     val gender: String,
     val phoneNumber: String,
     val role: String,
+    val email: String
 ){
-    constructor() : this("", "", "", "", "", "")
+    constructor() : this("", "", "", "", "", "","")
 }
 
 enum class Gender{
