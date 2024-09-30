45b37ca14636d097b874dba1bfa6d9ca371dfd23
mikesplore <mikepremium8@gmail.com>
2024-09-30 13:55:40 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/model/roomDatabase/Application.kt
+++ app/src/main/java/com/mike/hms/model/roomDatabase/Application.kt
@@ -3,11 +3,11 @@ package com.mike.hms.model.roomDatabase
 import android.app.Application
 import com.mike.hms.model.houseModel.HouseRepository
 import com.mike.hms.model.review.ReviewRepository
-import com.mike.hms.model.tenantModel.TenantRepository
+import com.mike.hms.model.userModel.UserRepository
 
 class HostelManagementSystemApp () : Application() {
     val database by lazy { HMSDatabase.getDatabase(this) }
-    val tenantRepository by lazy { TenantRepository(database.tenantDao()) }
+    val userRepository by lazy { UserRepository(database.userDao()) }
     val houseRepository by lazy { HouseRepository(database.houseDao()) }
     val reviewRepository by lazy { ReviewRepository(database.reviewDao()) }
 
