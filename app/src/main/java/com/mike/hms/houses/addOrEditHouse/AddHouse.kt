738c9e0c5ed2c68efd7be4492b556963af50c9e1
mikesplore <mikepremium8@gmail.com>
2024-10-17 10:42:50 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/houses/addOrEditHouse/AddHouse.kt
+++ app/src/main/java/com/mike/hms/houses/addOrEditHouse/AddHouse.kt
@@ -1,4 +1,4 @@
-package com.mike.hms.houses.addHouse
+package com.mike.hms.houses.addOrEditHouse
 
 import android.content.Context
 import android.util.Log
@@ -9,18 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.InputChip
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -31,8 +24,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.mike.hms.model.getHouseViewModel
 import com.mike.hms.model.houseModel.HouseAmenities
