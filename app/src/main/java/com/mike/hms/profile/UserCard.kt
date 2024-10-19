1ee34daccf634f33026b346289b0a76dc1917757
mikesplore <mikepremium8@gmail.com>
2024-10-19 11:38:40 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/profile/UserCard.kt
+++ app/src/main/java/com/mike/hms/profile/UserCard.kt
@@ -1,21 +1,36 @@
 package com.mike.hms.profile
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.mike.hms.model.userModel.UserEntity
 import com.mike.hms.ui.theme.CommonComponents as CC
 
@@ -23,6 +38,10 @@ import com.mike.hms.ui.theme.CommonComponents as CC
 fun UserCard(
     user: UserEntity,
 ) {
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val screenHeight = configuration.screenHeightDp.dp
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -32,6 +51,23 @@ fun UserCard(
             )
             .padding(16.dp)
     ) {
+        //Image Box
+        Box(
+            modifier = Modifier
+                .size(screenHeight * 0.2f)
+                .border(
+                    1.dp, CC.textColor(), CircleShape
+                )
+        ){
+            AsyncImage(
+                model = user.photoUrl,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .fillMaxSize()
+            )
+        }
         // Name Section
         Text(
             text = "Name",
@@ -100,17 +136,14 @@ fun UserCard(
 
 
 @Composable
-fun EditDetails(
-    firstName: String,
-    onFirstNameChange: (String) -> Unit,
-    lastName: String,
-    onLastNameChange: (String) -> Unit,
-    phoneNumber: String,
-    onPhoneNumberChange: (String) -> Unit,
-){
+fun EditDetails(){
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+    var phoneNumber by remember { mutableStateOf("") }
+
     CC.MyOutlinedTextField(
         value = firstName,
-        onValueChange = { onFirstNameChange(it) },
+        onValueChange = { firstName = it },
         label = "First Name",
         modifier = Modifier.fillMaxWidth(),
         placeholder = "Enter First Name"
@@ -118,7 +151,7 @@ fun EditDetails(
     Spacer(modifier = Modifier.height(8.dp))
     CC.MyOutlinedTextField(
         value = lastName,
-        onValueChange = { onLastNameChange(it) },
+        onValueChange = { lastName = it },
         label = "Last Name",
         modifier = Modifier.fillMaxWidth(),
         placeholder = "Enter Last Name"
@@ -126,10 +159,34 @@ fun EditDetails(
     Spacer(modifier = Modifier.height(8.dp))
     CC.MyOutlinedTextField(
         value = phoneNumber,
-        onValueChange = { onPhoneNumberChange(it) },
+        onValueChange = { phoneNumber = it },
         label = "Phone Number",
         keyboardType = KeyboardType.Phone,
         placeholder = "Enter Phone Number",
         modifier = Modifier.fillMaxWidth()
     )
+    Spacer(modifier = Modifier.height(8.dp))
+
+    CC.MyOutlinedTextField(
+        value = "",
+        onValueChange = {  },
+        label = "Email",
+        keyboardType = KeyboardType.Email,
+        placeholder = "Enter Email",
+        modifier = Modifier.fillMaxWidth()
+    )
+    Spacer(modifier = Modifier.height(8.dp))
+
+    Button(
+        onClick = { /* Handle save button click */ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = CC.primaryColor(),
+            contentColor = CC.surfaceContainerColor()
+        )
+    ) {
+        Text("Save", style = CC.contentTextStyle())
+    }
 }
