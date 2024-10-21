0e2b38dfb8842a3db2d323c8b24f81aa870d9ec6
mikesplore <mikepremium8@gmail.com>
2024-10-21 14:09:59 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/ui/theme/Color.kt
+++ app/src/main/java/com/mike/hms/ui/theme/Color.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Black = Color(0xFF212A3E)
 val White = Color(0xffCDE8E5)
 val Blue = Color (0XFF3FA2F6)
-val Red = Color(0XFFD20062)
+val Red = Color(0XFF164863)
 val WhiteSchemeTextColor = Color(0xff526D82)
 val BlackSchemeTextColor = Color(0xFF9DB2BF)
 val Green = Color(0xFF76B39D)
