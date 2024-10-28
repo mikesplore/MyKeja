735d74d3aa544dfe9fe905b070f2baed05d10331
mikesplore <mikepremium8@gmail.com>
2024-10-28 14:23:41 +0300

    Ui implementation

--- app/src/main/java/com/mike/hms/profile/AuthenticatedUser.kt
+++ app/src/main/java/com/mike/hms/profile/AuthenticatedUser.kt
@@ -2,6 +2,9 @@ package com.mike.hms.profile
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,9 +15,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -29,6 +35,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.mike.hms.HMSPreferences
 import com.mike.hms.model.getUserViewModel
 import com.mike.hms.model.userModel.CreditCardEntity
@@ -42,27 +49,23 @@ fun AuthenticatedUser(
     context: Context = LocalContext.current
 ) {
     val userViewModel = getUserViewModel(context)
-    val userID = HMSPreferences.userId.value
+    val userID = "User7"
     val user by userViewModel.user.observeAsState()
     val creditCard by userViewModel.creditCard.observeAsState()
 
     LaunchedEffect(Unit) {
-        Toast.makeText(context, "fetching user with user id: $userID", Toast.LENGTH_SHORT).show()
         userViewModel.getCreditCard(userID)
+        Toast.makeText(context,"Searching credit card of user $userID", Toast.LENGTH_SHORT).show()
         userViewModel.getUserByID(userID)
-        Toast.makeText(context,"${user?.firstName}", Toast.LENGTH_SHORT).show()
     }
 
 
     Column(
         modifier = Modifier
-            .verticalScroll(rememberScrollState())
             .imePadding()
             .fillMaxSize()
             .padding(16.dp)
     ) {
-
-
         // User Details Section
         Text(
             text = "Personal Information",
@@ -78,40 +81,25 @@ fun AuthenticatedUser(
         } else {
             user?.let { UserCard(it) }
         }
-
-        Spacer(modifier = Modifier.height(32.dp))
-        creditCard?.let {
-            CreditCard(
-                creditCardWithUser = it
-            )
-        }
-
-        // Payment Method Section
+        Spacer(modifier = Modifier.height(20.dp))
+        // Credit Card Section
         Text(
-            text = "Payment Method",
+            text = "Credit Card Information",
             style = CC.titleTextStyle(),
             modifier = Modifier.align(Alignment.Start)
         )
         Spacer(modifier = Modifier.height(16.dp))
-
-        // Payment Method
-        if (creditCard != null) {
-
+        if (creditCard == null) {
+            Text("You don't have a saved credit card yet", style = CC.contentTextStyle())
+            Spacer(modifier = Modifier.height(16.dp))
+            AddCreditCard(context, userViewModel)
+            Spacer(modifier = Modifier.height(20.dp))
+        } else {
+            CreditCard(creditCard!!)
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Save Payment Method Button
-        if (paymentMethod.isNotEmpty()) {
-            Button(
-                onClick = {
-                    // TODO: Implement logic to save payment method
-                },
-                modifier = Modifier.align(Alignment.End)
-            ) {
-                Text("Save Payment Method")
-            }
-        }
+        Spacer(modifier = Modifier.height(20.dp))
+        // Danger Zone Section
+        DangerZone(userViewModel, context, navController = NavController(context))
     }
 }
 
@@ -126,55 +114,275 @@ fun AddCreditCard(
     var cvv by remember { mutableStateOf("") }
     val userID = HMSPreferences.userId.value
 
-    Text("Add Credit Card", style = CC.titleTextStyle())
-    CC.MyOutlinedTextField(
-        value = cardNumber,
-        onValueChange = { cardNumber = it },
-        label = "Card Number",
-        keyboardType = KeyboardType.Number,
-        modifier = Modifier.fillMaxWidth(),
-        placeholder = "1234 5678 9012 3456"
-    )
-    Spacer(modifier = Modifier.height(8.dp))
-    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-        CC.MyOutlinedTextField(
-            value = expiryDate,
-            onValueChange = { expiryDate = it },
-            label = "Expiry Date",
-            keyboardType = KeyboardType.Number,
-            modifier = Modifier.weight(1f),
-            placeholder = "MM/YY"
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        CC.MyOutlinedTextField(
-            value = cvv,
-            onValueChange = { cvv = it },
-            label = "CVV",
-            keyboardType = KeyboardType.Number,
-            modifier = Modifier.weight(1f),
-            placeholder = "123"
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Button(onClick = {
-            val card = CreditCardEntity(
-                userId = userID,
-                cardNumber = cardNumber,
-                expiryDate = expiryDate,
-                cvv = cvv
+    var cardNumberError by remember { mutableStateOf(false) }
+    var expiryDateError by remember { mutableStateOf(false) }
+    var cvvError by remember { mutableStateOf(false) }
+    var showAddCard by remember { mutableStateOf(false) }
+
+    fun validateInput(): Boolean {
+        var isValid = true
+        cardNumberError = cardNumber.length != 16
+        expiryDateError = expiryDate.isEmpty() || !expiryDate.matches("\\d{2}/\\d{2}".toRegex())
+        cvvError = cvv.length != 3
+
+        isValid = !cardNumberError && !expiryDateError && !cvvError
+        return isValid
+    }
+
+    fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    AnimatedVisibility(visible = !showAddCard) {
+        Button(
+            onClick = {
+                userViewModel.getCreditCard(userID)
+               // showAddCard = !showAddCard
+            },
+            colors = CC.buttonColors(),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text("Add Credit Card", style = CC.contentTextStyle().copy(color = CC.primaryColor()))
+        }
+    }
+
+    AnimatedVisibility(visible = showAddCard) {
+        Column(
+            modifier = Modifier
+                .border(
+                    width = 1.dp,
+                    color = CC.textColor(),
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Add Credit Card",
+                style = CC.titleTextStyle(),
+                modifier = Modifier.padding(bottom = 16.dp)
             )
-            userViewModel.insertCreditCard(card) {
-                if (it) {
-                    Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
+
+            CreditCardTextField(
+                value = cardNumber,
+                onValueChange = { cardNumber = it },
+                label = "Card Number",
+                keyboardType = KeyboardType.Number,
+                placeholder = "1234 5678 9012 3456",
+                isError = cardNumberError,
+                errorMessage = "Invalid card number"
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row(
+                Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                CreditCardTextField(
+                    value = expiryDate,
+                    onValueChange = { expiryDate = it },
+                    label = "Expiry Date",
+                    keyboardType = KeyboardType.Unspecified,
+                    placeholder = "MM/YY",
+                    modifier = Modifier.weight(1f),
+                    isError = expiryDateError,
+                    errorMessage = "Invalid expiry date"
+                )
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                CreditCardTextField(
+                    value = cvv,
+                    onValueChange = { cvv = it },
+                    label = "CVV",
+                    placeholder = "123",
+                    keyboardType = KeyboardType.Number,
+                    modifier = Modifier.weight(1f),
+                    isError = cvvError,
+                    errorMessage = "Invalid CVV"
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Button(
+                    onClick = {
+                        if (validateInput()) {
+                            CC.generateCardId { id ->
+                            val card = CreditCardEntity(
+                                cardId = id,
+                                userId = userID,
+                                cardNumber = cardNumber,
+                                expiryDate = expiryDate,
+                                cvv = cvv
+                            )
+                            userViewModel.insertCreditCard(card) { success ->
+                                if (success) {
+                                    showToast("Success!")
+                                } else {
+                                    showToast("Failure!")
+                                }
+                            }}
+                        }
+                    },
+                    shape = RoundedCornerShape(8.dp),
+                    colors = CC.buttonColors()
+                ) {
+                    Text(
+                        "Save Card",
+                        style = CC.contentTextStyle()
+                            .copy(fontWeight = FontWeight.Bold, color = CC.primaryColor())
+                    )
+                }
+                OutlinedButton(
+                    onClick = {
+                        showAddCard = !showAddCard
+                    },
+                    shape = RoundedCornerShape(8.dp),
+                ) {
+                    Text(
+                        "Cancel",
+                        style = CC.contentTextStyle().copy(fontWeight = FontWeight.Bold)
+                    )
                 }
             }
-        }) {
-            Text("Save Card", style = CC.contentTextStyle().copy(fontWeight = FontWeight.Bold))
+        }
+
+    }
+}
+
+@Composable
+fun CreditCardTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    keyboardType: KeyboardType,
+    placeholder: String,
+    modifier: Modifier = Modifier,
+    isError: Boolean = false,
+    errorMessage: String = ""
+) {
+    Column(modifier = modifier) {
+        CC.MyOutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            label = label,
+            keyboardType = keyboardType,
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = placeholder,
+            isError = isError
+        )
+        if (isError) {
+            Text(
+                text = errorMessage,
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(start = 16.dp, top = 4.dp)
+            )
         }
     }
 }
 
+
+@Composable
+fun DangerZone(userViewModel: UserViewModel, context: Context, navController: NavController) {
+    var showDeleteDialog by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+
+        if (showDeleteDialog) {
+            ShowDeleteDialog(
+                onDismiss = { showDeleteDialog = it },
+                userViewModel = userViewModel,
+                context = context
+            )
+        }
+        Text("Delete Account", style = CC.contentTextStyle())
+        Button(
+            onClick = {
+                showDeleteDialog = !showDeleteDialog
+
+            },
+            colors = CC.buttonColors().copy(),
+            shape = RoundedCornerShape(8.dp),
+        ) {
+            Text("Delete Account", style = CC.contentTextStyle())
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShowDeleteDialog(
+    onDismiss: (Boolean) -> Unit,
+    userViewModel: UserViewModel,
+    context: Context
+
+) {
+    val userID = HMSPreferences.userId.value
+    BasicAlertDialog(
+        onDismissRequest = {onDismiss(false)}
+    ) {
+        Column(
+            modifier = Modifier
+                .border(
+                    width = 1.dp,
+                    color = CC.textColor(),
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .background(CC.primaryColor())
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text("Are you sure you want to delete your account?", style = CC.contentTextStyle())
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                OutlinedButton(
+                    onClick = { onDismiss(false) },
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Text("Cancel", style = CC.contentTextStyle())
+                }
+                Button(
+                    onClick = {
+                        userViewModel.deleteUser(userID){ success ->
+                            if (success){
+                                Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
+                                onDismiss(true)
+                            } else {
+                                Toast.makeText(context, "Failed, please try again", Toast.LENGTH_SHORT).show()
+                                onDismiss(false)
+                            }
+
+                        }
+
+                    },
+                    enabled = false,
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Text("Delete", style = CC.contentTextStyle())
+                }
+            }
+        }
+    }
+
+}
 
 
 
