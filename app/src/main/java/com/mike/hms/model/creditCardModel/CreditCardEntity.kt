04dec52713e008d51f8f013d814311807150235c
mikesplore <mikepremium8@gmail.com>
2024-10-29 16:19:13 +0300

    Revisiting the Backend

---
+++ app/src/main/java/com/mike/hms/model/creditCardModel/CreditCardEntity.kt
@@ -0,0 +1,30 @@
+package com.mike.hms.model.creditCardModel
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+import com.mike.hms.model.userModel.UserEntity
+
+@Entity(tableName = "credit_cards")
+data class CreditCardEntity(
+    @PrimaryKey val cardId: String,
+    val userId: String = "",
+    val cardNumber: String = "",
+    val expiryDate: String = "",
+    val cvv: String = ""
+){
+    constructor() : this("", "", "", "", "")
+}
+
+data class CreditCardWithUser(
+    @Embedded val creditCard: CreditCardEntity,
+    @Relation(
+        parentColumn = "userId", // Column in CreditCard
+        entityColumn = "userID" // Column in UserEntity
+    )
+    val user: UserEntity
+){
+    constructor() : this(CreditCardEntity(), UserEntity())
+
+}
