d85584ddfa8936f3895b758c456a78a8662d4bfc
mikesplore <mikepremium8@gmail.com>
2024-10-29 16:18:47 +0300

    Revisiting the Backend

--- app/src/main/java/com/mike/hms/model/userModel/UserEntity.kt
+++ app/src/main/java/com/mike/hms/model/userModel/UserEntity.kt
@@ -19,25 +19,3 @@ data class UserEntity(
     constructor() : this("", "", "", "", "", "","","")
 }
 
-@Entity(tableName = "credit_cards")
-data class CreditCardEntity(
-    @PrimaryKey val cardId: String,
-    val userId: String,
-    val cardNumber: String,
-    val expiryDate: String,
-    val cvv: String
-){
-    constructor() : this("", "", "", "", "")
-}
-
-data class CreditCardWithUser(
-    @Embedded val creditCard: CreditCardEntity,
-    @Relation(
-        parentColumn = "userId", // Column in CreditCard
-        entityColumn = "userID" // Column in UserEntity
-    )
-    val user: UserEntity
-){
-    constructor() : this(CreditCardEntity(), UserEntity())
-
-}
