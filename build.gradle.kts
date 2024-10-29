dd93f391195c6c59b80003071319ee3269cb91fd
mikesplore <mikepremium8@gmail.com>
2024-10-29 16:17:52 +0300

    Revisiting the Backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.google.gms.google.services) apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
 }
