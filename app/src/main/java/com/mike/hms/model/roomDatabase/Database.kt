c600dc687d4c869575d5338036be031b6f6d9fde
mikesplore <mikepremium8@gmail.com>
2024-10-29 16:17:38 +0300

    Revisiting the Backend

--- app/src/main/java/com/mike/hms/model/roomDatabase/Database.kt
+++ app/src/main/java/com/mike/hms/model/roomDatabase/Database.kt
@@ -1,15 +1,14 @@
 package com.mike.hms.model.roomDatabase
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.mike.hms.model.creditCardModel.CreditCardDao
+import com.mike.hms.model.creditCardModel.CreditCardEntity
 import com.mike.hms.model.houseModel.HouseDao
 import com.mike.hms.model.houseModel.HouseEntity
 import com.mike.hms.model.review.ReviewDao
 import com.mike.hms.model.review.ReviewEntity
-import com.mike.hms.model.userModel.CreditCardEntity
 import com.mike.hms.model.userModel.UserDao
 import com.mike.hms.model.userModel.UserEntity
 
@@ -28,23 +27,5 @@ abstract class HMSDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun houseDao(): HouseDao
     abstract fun reviewDao(): ReviewDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: HMSDatabase? = null
-
-        fun getDatabase(context: Context): HMSDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    HMSDatabase::class.java,
-                    "MyDatabase"
-                ).fallbackToDestructiveMigration()
-                    .build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
+    abstract fun creditCardDao(): CreditCardDao
 }
-
