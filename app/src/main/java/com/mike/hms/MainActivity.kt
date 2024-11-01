37c6a5fb1971a1de615f66060692ad99ecf6236d
mikesplore <mikepremium8@gmail.com>
2024-11-01 16:19:23 +0300

    Continuation

--- app/src/main/java/com/mike/hms/MainActivity.kt
+++ app/src/main/java/com/mike/hms/MainActivity.kt
@@ -6,10 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.mike.hms.model.favorites.FavoriteViewModel
+import com.mike.hms.model.houseModel.HouseViewModel
 import com.mike.hms.ui.theme.HostelManagementSystemTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -45,13 +50,21 @@ class MainActivity : AppCompatActivity() {
                 darkTheme = HMSPreferences.darkMode.value,
                 dynamicColor = false
             ) {
+                val houseViewModel: HouseViewModel = hiltViewModel()
+                val houses = houseViewModel.houses.collectAsState()
+                val favoriteViewModel: FavoriteViewModel = hiltViewModel()
                 val window = (LocalView.current.context as Activity).window
                 WindowCompat.getInsetsController(
                     window,
                     window.decorView
                 ).isAppearanceLightStatusBars = !HMSPreferences.darkMode.value
+
+                LaunchedEffect(Unit) {
+                    houseViewModel.getAllHouses()
+                    favoriteViewModel.fetchFavoriteHouses()
+                }
                 // Your content goes here
-                NavGraph(this)
+                NavGraph(houseViewModel, houses.value, favoriteViewModel, this)
             }
         }
     }
