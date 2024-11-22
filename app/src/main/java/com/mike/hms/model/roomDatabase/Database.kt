3df4d81b8050f0d9172cdf2b24ed629a3c85a72b
mikesplore <mikepremium8@gmail.com>
2024-11-22 09:57:49 +0300

    UI implementation

--- app/src/main/java/com/mike/hms/model/roomDatabase/Database.kt
+++ app/src/main/java/com/mike/hms/model/roomDatabase/Database.kt
@@ -3,12 +3,16 @@ package com.mike.hms.model.roomDatabase
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.mike.hms.model.creditCardModel.CreditCardDao
-import com.mike.hms.model.creditCardModel.CreditCardEntity
+import com.mike.hms.model.paymentMethods.CreditCardDao
+import com.mike.hms.model.paymentMethods.CreditCardEntity
 import com.mike.hms.model.favorites.FavoriteDao
 import com.mike.hms.model.favorites.FavouriteEntity
 import com.mike.hms.model.houseModel.HouseDao
 import com.mike.hms.model.houseModel.HouseEntity
+import com.mike.hms.model.paymentMethods.MpesaDao
+import com.mike.hms.model.paymentMethods.MpesaEntity
+import com.mike.hms.model.paymentMethods.PayPalDao
+import com.mike.hms.model.paymentMethods.PayPalEntity
 import com.mike.hms.model.review.ReviewDao
 import com.mike.hms.model.review.ReviewEntity
 import com.mike.hms.model.userModel.UserDao
@@ -20,7 +24,9 @@ import com.mike.hms.model.userModel.UserEntity
         HouseEntity::class,
         ReviewEntity::class,
         CreditCardEntity::class,
-        FavouriteEntity:: class
+        FavouriteEntity:: class,
+        PayPalEntity::class,
+        MpesaEntity::class
     ],
     version = 2,
     exportSchema = false
@@ -32,5 +38,7 @@ abstract class HMSDatabase : RoomDatabase() {
     abstract fun reviewDao(): ReviewDao
     abstract fun creditCardDao(): CreditCardDao
     abstract fun favoriteDao(): FavoriteDao
+    abstract fun payPalDao(): PayPalDao
+    abstract fun mpesaDao(): MpesaDao
 
 }
