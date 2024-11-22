dd0fc9b9e2f5399f506efdee591b1bb8875275b2
mikesplore <mikepremium8@gmail.com>
2024-11-22 09:57:43 +0300

    UI implementation

---
+++ app/src/main/java/com/mike/hms/model/paymentMethods/MpesaEntity.kt
@@ -0,0 +1,30 @@
+package com.mike.hms.model.paymentMethods
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+import com.mike.hms.model.userModel.UserEntity
+
+@Entity(tableName = "mpesa")
+data class MpesaEntity(
+    @PrimaryKey val mpesaId: String,
+    val userId: String,
+    val phoneNumber: String,
+    val amount: Double,
+    val transactionId: String
+){
+    constructor() : this("", "", "", 0.0, "")
+
+}
+
+data class MpesaWithUser(
+    @Embedded val mpesa: MpesaEntity,
+    @Relation(
+        parentColumn = "userId", // Column in MpesaEntity
+        entityColumn = "userID" // Column in UserEntity
+    )
+    val user: UserEntity
+) {
+    constructor() : this(MpesaEntity(), UserEntity())
+}
