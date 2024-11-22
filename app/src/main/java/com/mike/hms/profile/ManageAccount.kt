d0a7402f4704826c24748dee22e8db9dbe4ed8a5
mikesplore <mikepremium8@gmail.com>
2024-11-22 16:29:25 +0300

    UI implementation

--- app/src/main/java/com/mike/hms/profile/ManageAccount.kt
+++ app/src/main/java/com/mike/hms/profile/ManageAccount.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.input.KeyboardActionHandler
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
 import androidx.compose.material.icons.filled.Delete
@@ -34,6 +35,8 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
@@ -44,10 +47,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -58,6 +63,7 @@ import com.mike.hms.HMSPreferences
 import com.mike.hms.model.userModel.UserEntity
 import com.mike.hms.model.userModel.UserViewModel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import com.mike.hms.ui.theme.CommonComponents as CC
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -66,12 +72,16 @@ fun ManageAccount(userViewModel: UserViewModel, context: Context, navController:
     val user by userViewModel.user.collectAsState()
     val auth = FirebaseAuth.getInstance()
     val email = auth.currentUser?.email
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     LaunchedEffect(Unit) {
         userViewModel.getUserByEmail(email!!)
     }
 
     Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
             TopAppBar(
                 title = { Text("Manage Account", style = CC.titleTextStyle()) },
@@ -95,7 +105,22 @@ fun ManageAccount(userViewModel: UserViewModel, context: Context, navController:
                 .fillMaxSize()
 
         ) {
-            user?.let { EditDetails(it, userViewModel) }
+            user?.let { EditDetails(it, userViewModel){
+                if (it){
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar("Details updated successfully", actionLabel = "Dismiss")
+                        keyboardController?.hide()
+                        snackbarHostState.currentSnackbarData?.dismiss()
+
+                    }
+                }
+                else {
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar("Failed to update details")
+                        snackbarHostState.currentSnackbarData?.dismiss()
+                    }
+                }
+            } }
             Spacer(modifier = Modifier.height(16.dp))
             HorizontalDivider(
                 color = CC.textColor().copy(alpha = 0.5f),
@@ -135,86 +160,92 @@ fun ManageAccount(userViewModel: UserViewModel, context: Context, navController:
 }
 
 @Composable
-fun EditDetails(user: UserEntity, userViewModel: UserViewModel){
+fun EditDetails(user: UserEntity, userViewModel: UserViewModel, feedback:(Boolean) -> Unit) {
     var firstName by remember { mutableStateOf(user.firstName) }
     var lastName by remember { mutableStateOf(user.lastName) }
     var phoneNumber by remember { mutableStateOf(user.phoneNumber) }
     val email by remember { mutableStateOf(user.email) }
-
-    Column(
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-    ) {
-        Text(
-            text = "Edit Details",
-            style = CC.titleTextStyle().copy(
-                color = CC.secondaryColor(),
-                fontWeight = FontWeight.Bold,
-            ),
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        CC.MyOutlinedTextField(
-            value = firstName,
-            onValueChange = { firstName = it },
-            label = "First Name",
-            modifier = Modifier.fillMaxWidth(),
-            placeholder = "Enter First Name"
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-
-        CC.MyOutlinedTextField(
-            value = lastName,
-            onValueChange = { lastName = it },
-            label = "Last Name",
-            modifier = Modifier.fillMaxWidth(),
-            placeholder = "Enter Last Name"
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-
-        CC.MyOutlinedTextField(
-            value = phoneNumber,
-            onValueChange = { phoneNumber = it },
-            label = "Phone Number",
-            keyboardType = KeyboardType.Phone,
-            placeholder = "Enter Phone Number",
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-
-        CC.MyOutlinedTextField(
-            value = email,
-            onValueChange = {},
-            label = "Email",
-            keyboardType = KeyboardType.Email,
-            placeholder = "Enter Email",
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Button(
-            onClick = {
-                val updatedUser = user.copy(
-                    firstName = firstName,
-                    lastName = lastName,
-                    phoneNumber = phoneNumber
-                )
-                userViewModel.insertUser(updatedUser){
-
-                }
-            },
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = CC.secondaryColor(),
-            )
+                .padding(horizontal = 16.dp)
         ) {
-            Text("Save", style = CC.contentTextStyle().copy(color = CC.primaryColor()))
+            Text(
+                text = "Edit Details",
+                style = CC.titleTextStyle().copy(
+                    color = CC.secondaryColor(),
+                    fontWeight = FontWeight.Bold,
+                ),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            CC.MyOutlinedTextField(
+                value = firstName,
+                onValueChange = { firstName = it },
+                label = "First Name",
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = "Enter First Name"
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            CC.MyOutlinedTextField(
+                value = lastName,
+                onValueChange = { lastName = it },
+                label = "Last Name",
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = "Enter Last Name"
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            CC.MyOutlinedTextField(
+                value = phoneNumber,
+                onValueChange = { phoneNumber = it },
+                label = "Phone Number",
+                keyboardType = KeyboardType.Phone,
+                placeholder = "Enter Phone Number",
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            CC.MyOutlinedTextField(
+                value = email,
+                onValueChange = {},
+                label = "Email",
+                keyboardType = KeyboardType.Email,
+                placeholder = "Enter Email",
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Button(
+                onClick = {
+                    val updatedUser = user.copy(
+                        firstName = firstName,
+                        lastName = lastName,
+                        phoneNumber = phoneNumber
+                    )
+                    userViewModel.insertUser(updatedUser) { success ->
+                        if (success) {
+                            feedback(true)
+
+                        } else {
+                            feedback(false)
+                        }
+
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = CC.secondaryColor(),
+                )
+            ) {
+                Text("Save", style = CC.contentTextStyle().copy(color = CC.primaryColor()))
+            }
+            Spacer(modifier = Modifier.height(8.dp))
         }
-        Spacer(modifier = Modifier.height(8.dp))
-    }
 }
+
 
 
 
