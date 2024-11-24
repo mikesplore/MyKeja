9aa46280e2f2e61142a4a939876265d31330cdb1
mikesplore <mikepremium8@gmail.com>
2024-11-24 12:01:30 +0300

    UI implementation

--- app/src/main/java/com/mike/hms/profile/DangerZone.kt
+++ app/src/main/java/com/mike/hms/profile/DangerZone.kt
@@ -9,12 +9,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
