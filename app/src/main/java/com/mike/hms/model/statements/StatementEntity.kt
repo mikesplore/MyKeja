8c8452c0d43c1b20f966cba02e208ccba7eede13
mikesplore <mikepremium8@gmail.com>
2024-11-24 18:28:31 +0300

    UI implementation

--- app/src/main/java/com/mike/hms/model/statements/StatementEntity.kt
+++ app/src/main/java/com/mike/hms/model/statements/StatementEntity.kt
@@ -3,20 +3,26 @@ package com.mike.hms.model.statements
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "statement")
-data class StatementEntity(
-    @PrimaryKey val statementID: String,
+@Entity(tableName = "transaction")
+data class TransactionEntity(
+    @PrimaryKey val transactionID: String,
+    val transactionNo: String,
     val paymentMethod: PaymentMethod,
     val amount: String,
     val date: String,
-    val houseID: String,
-    val userId: String
+    val userId: String,
+    val transactionType: TransactionType
 ){
-    constructor(): this("", PaymentMethod.CREDIT_CARD,"","","","")
+    constructor(): this("","", PaymentMethod.CREDIT_CARD, "", "", "", TransactionType.ADDITION)
 }
 
 enum class PaymentMethod {
     CREDIT_CARD,
     MPESA,
     PAYPAL
+}
+
+enum class TransactionType {
+    ADDITION,
+    SUBTRACTION
 }
